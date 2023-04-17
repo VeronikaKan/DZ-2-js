9b032d1d65e83ada976a3fe5b1b82d784751d6e4
VeronikaKan <kan.v98@gmail.com>
2023-04-17 22:37:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+// let str = "LIGHTCODE PROGRAMMING"
+// console.log(str.split(" ")[0].length);
+
+// let str = "Максимально УЖАСНО"
+// console.log(str.split(" ")[1].toLowerCase());
+
+
+// let str = "максимально УЖАСНО"
+// console.log(str.split(" ")[0].toUpperCase());
+
+// let str = "Максимально Ужасно"
+// console.log(str.slice(5 , 17));
+
+// let str = "Максимально"
+// let str2 = "Ужасно"
+// console.log(`${str} ${str2}`);
+
+// let str = "Максимально Ужасно"
+// console.log(str.indexOf("Ужасно"));
+
+// let str = "Максимально Ужасно"
+// if(str.indexOf("а")!== -1){
+//     console.log("встречается более 1 раза");
+// }else {
+//     console.log("встречается менее одного раза");
+// }
+
+// let str = "Максимально Ужасно"
+// console.log(str.indexOf("а"));
+
+// let str = "Максимально Ужасно"
+// console.log(str.lastIndexOf("о"))
+
+
+let str = 'aaa bbb ccc'
+console.log(str.substr(4, 3))
+console.log(str.substring(4, 7))
+console.log(str.slice(4, 7));
